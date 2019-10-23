52bcef078da0c8fc9eecf7959d6eebc8c2e073bc
Marat <yeffasol@mail.ru>
2019-10-23 21:46:41 +0300

    For loops

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue!'
+		todos: [
+			{ text: 'Learn JavaScript' },
+			{ text: 'Learn Vue' },
+			{ text: 'Build something awesome' }
+		]
 	}
 });
