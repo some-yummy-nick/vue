73dbce87992c40f657c413d34c5127bde0248951
Marat <yeffasol@mail.ru>
2019-10-24 20:05:20 +0300

    Components

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+Vue.component('todo-item', {
+	props: ['todo'],
+	template: '<li>{{ todo.text }}</li>'
+});
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue!'
+		groceryList: [
+			{ id: 0, text: 'Vegetables' },
+			{ id: 1, text: 'Cheese' },
+			{ id: 2, text: 'Whatever else humans are supposed to eat' }
+		]
 	}
 });
